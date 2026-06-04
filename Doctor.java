72316e04c8183004f8a9d72b00844e302bd8f82a
Alejandro Amador <diegoaar.537@gmail.com>
2026-06-03 19:03:36 -0600

    agregando Doctor.java

---
+++ Doctor.java
@@ -0,0 +1,51 @@
+
+
+public class Doctor {
+
+    private String especialidad;
+    private String universidad;
+    private int anioobtencion;
+
+    public Doctor(String especialidad, String universiad, int anioobtencion) {
+        this.especialidad = especialidad;
+        this.universidad = universiad;
+        this.anioobtencion = anioobtencion;
+
+    }
+
+    //metodos propios 
+    public boolean validartitulo() {
+        System.out.println("titulo de doctorado en " + especialidad + " obtenido en la universiad " + universidad);
+        return true;
+    }
+
+    public void registrartitulo() {
+        System.out.println("el titulo fue obtenido en el año " + anioobtencion);
+    }
+
+    //getter y setter
+    public String getEspecialidad() {
+        return especialidad;
+    }
+
+    public void setEspecialidad(String especialidad) {
+        this.especialidad = especialidad;
+    }
+
+    public String getUniversidad() {
+        return universidad;
+    }
+
+    public void setUniversidad(String universidad) {
+        this.universidad = universidad;
+    }
+
+    public int getAnioobtencion() {
+        return anioobtencion;
+    }
+
+    public void setAnioobtencion(int anioobtencion) {
+        this.anioobtencion = anioobtencion;
+    }
+
+}

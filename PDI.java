aa9ecbe92bffa8049cfbe21535a632a891e0f89d
Alejandro Amador <diegoaar.537@gmail.com>
2026-06-03 18:16:40 -0600

    agregando PDI.java

---
+++ PDI.java
@@ -0,0 +1,56 @@
+
+import java.time.LocalDate;
+
+public class PDI extends Trabajador {
+
+    private String categoria;
+    private String especialidad;
+    private int horasDocencia;
+
+    public PDI(String nombre, String direccion, LocalDate fechanacimiento, String puesto, double salario, LocalDate fechaingreso, String categoria, String especialidad, int horasDocencia) {
+        super(nombre, direccion, fechanacimiento, fechaingreso, salario, puesto);
+        this.categoria = categoria;
+        this.especialidad = especialidad;
+        this.horasDocencia = horasDocencia;
+    }
+
+    //metodos
+    public void investigar(String tema) {
+        System.out.println("el profesor " + getnombre() + " investiga el tema " + tema);
+    }
+
+    public void imparticlase() {
+        System.out.println("el profesor " + getnombre() + " tiene asignadas " + horasDocencia + " horas asignadas");
+    }
+
+    //getters y setters
+    //categoria 
+    public String getcategotia() {
+        return categoria;
+
+    }
+
+    public void setcategoria(String categoria) {
+        this.categoria = categoria;
+
+    }
+
+    //especialidad
+    public String getespecialidad() {
+        return especialidad;
+    }
+
+    public void setespecialidad(String especialidad) {
+        this.especialidad = especialidad;
+    }
+
+    //horas de docencia
+    public int gethorasDocencia() {
+        return horasDocencia;
+
+    }
+
+    public void sethorasDocencia(int horasDocencia) {
+        this.horasDocencia = horasDocencia;
+    }
+}
